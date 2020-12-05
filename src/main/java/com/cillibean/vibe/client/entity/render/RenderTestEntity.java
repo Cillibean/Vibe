92f3b4dbd9f9c262abbb750dc1ee25b710be6102
Cillibean <cillibean16@gmail.com>
2020-12-05 19:05:49 +0000

    Initial commit

---
+++ src/main/java/com/cillibean/vibe/client/entity/render/RenderTestEntity.java
@@ -0,0 +1,24 @@
+package com.cillibean.vibe.client.entity.render;
+
+import com.cillibean.vibe.entities.TestEntity;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.model.PigModel;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.entity.model.SheepModel;
+import net.minecraft.client.renderer.entity.model.ShulkerModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderTestEntity extends MobRenderer<TestEntity, PigModel<TestEntity>> {
+
+    public RenderTestEntity(EntityRendererManager renderManagerIn, PigModel<TestEntity> entityModelIn, float shadowSizeIn) {
+        super(renderManagerIn, entityModelIn, shadowSizeIn);
+    }
+
+    @Override
+    public ResourceLocation getEntityTexture(TestEntity entity) {
+        return null;
+    }
+}
---
+++ src/main/java/com/cillibean/vibe/init/EventHandler.java
@@ -0,0 +1,9 @@
+package com.cillibean.vibe.init;
+
+import com.cillibean.vibe.VibeMod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = VibeMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class EventHandler {
+
+}
